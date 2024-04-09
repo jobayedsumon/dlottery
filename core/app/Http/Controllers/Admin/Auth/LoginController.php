8fcfe044339b6f6e9d5eac5190b7baefebff5e4c
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2024-04-09 19:39:29 +0600

    login controller fix

--- core/app/Http/Controllers/Admin/Auth/LoginController.php
+++ core/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Laramin\Utility\Onumoti;
 
 class LoginController extends Controller
 {
